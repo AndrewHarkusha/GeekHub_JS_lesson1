a4ade25033ba5eb5bd86c435029f0946219d0b70
Andrey Harkusha <xfeaton@gmail.com>
2015-10-11 21:15:26 +0300

    first file

---
+++ main.js
@@ -0,0 +1,15 @@
+		function test()
+		{
+			alert(x);
+			var x = 1;
+		}
+		
+		test = function()
+		{
+		var sign = "#";
+		for (var i = 0; i <= 7; i++)
+			{
+				console.log(sign + "<br>");
+				sign = sign + "#";
+			}
+		}	
