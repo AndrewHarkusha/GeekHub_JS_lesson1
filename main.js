1247ed1d3a0a14ccc3eb261e00497ff7b20758fb
Andrey Harkusha <xfeaton@gmail.com>
2015-10-11 21:34:24 +0300

    add throw error in start function

--- main.js
+++ main.js
@@ -4,17 +4,8 @@
 			if (x > 0) {
 				console.log("Good digit");
 			} else{
-				console.error("Error. Digit less then 1");
+				throw new Error("Error. Digit less then 1");
 			}
 			alert(x);
 		}
 		
-		test = function()
-		{
-		var sign = "#";
-		for (var i = 0; i <= 7; i++)
-			{
-				console.log(sign + "<br>");
-				sign = sign + "#";
-			}
-		}	
